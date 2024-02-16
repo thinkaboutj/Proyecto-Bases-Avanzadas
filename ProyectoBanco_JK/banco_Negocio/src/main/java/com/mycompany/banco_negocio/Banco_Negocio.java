bc7d9fb49a1dacd750ee8b6d02e5d328b7be9fc7
Jesus <159750556+thinkaboutj@users.noreply.github.com>
2024-02-16 00:26:06 -0700

    proyecto 4 capaz

---
+++ ProyectoBanco_JK/banco_Negocio/src/main/java/com/mycompany/banco_negocio/Banco_Negocio.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.banco_negocio;
+
+/**
+ *
+ * @author Jesus Gabriel
+ */
+public class Banco_Negocio {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ ProyectoBanco_JK/banco_interfaz/src/main/java/com/mycompany/banco_interfaz/Banco_interfaz.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.banco_interfaz;
+
+/**
+ *
+ * @author Jesus Gabriel
+ */
+public class Banco_interfaz {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
