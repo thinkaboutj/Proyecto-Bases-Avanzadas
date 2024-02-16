deb8a57f4fa6061d639d84833717b95f7510cdae
Jesus <Jesus_G_1710@hotmail.com>
2024-02-16 00:40:44 -0700

    actualize codigo

--- ProyectoBanco_JK/banco_Dominio/src/main/java/dominio/Cliente.java
+++ ProyectoBanco_JK/banco_Dominio/src/main/java/dominio/Cliente.java
@@ -4,10 +4,88 @@
  */
 package dominio;
 
+import java.util.Date;
+
 /**
  *
  * @author Jesus Gabriel
  */
 public class Cliente {
+    
+     private int id;
+    private Date fechaN;
+    private int edad;
+    private String nombre;
+    private String apellidoP;
+    private String apellidoM;
+    private int idDireccion;
+
+    // Constructor
+    public Cliente(int id, Date fechaN, int edad, String nombre, String apellidoP, String apellidoM, int idDireccion) {
+        this.id = id;
+        this.fechaN = fechaN;
+        this.edad = edad;
+        this.nombre = nombre;
+        this.apellidoP = apellidoP;
+        this.apellidoM = apellidoM;
+        this.idDireccion = idDireccion;
+    }
+
+    // Getters y Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getFechaN() {
+        return fechaN;
+    }
+
+    public void setFechaN(Date fechaN) {
+        this.fechaN = fechaN;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidoP() {
+        return apellidoP;
+    }
+
+    public void setApellidoP(String apellidoP) {
+        this.apellidoP = apellidoP;
+    }
+
+    public String getApellidoM() {
+        return apellidoM;
+    }
+
+    public void setApellidoM(String apellidoM) {
+        this.apellidoM = apellidoM;
+    }
+
+    public int getIdDireccion() {
+        return idDireccion;
+    }
+
+    public void setIdDireccion(int idDireccion) {
+        this.idDireccion = idDireccion;
+    }
  
 }
--- ProyectoBanco_JK/banco_Dominio/src/main/java/dominio/Cuenta.java
+++ ProyectoBanco_JK/banco_Dominio/src/main/java/dominio/Cuenta.java
@@ -4,10 +4,88 @@
  */
 package dominio;
 
+import java.util.Date;
+
 /**
  *
  * @author Jesus Gabriel
  */
 public class Cuenta {
+    
+    private int id;
+    private Date fecha;
+    private int numC;
+    private float saldo;
+    private String usuario;
+    private String contrasena;
+    private int idCliente;
+
+    // Constructor
+    public Cuenta(int id, Date fecha, int numC, float saldo, String usuario, String contrasena, int idCliente) {
+        this.id = id;
+        this.fecha = fecha;
+        this.numC = numC;
+        this.saldo = saldo;
+        this.usuario = usuario;
+        this.contrasena = contrasena;
+        this.idCliente = idCliente;
+    }
+
+    // Getters y Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public int getNumC() {
+        return numC;
+    }
+
+    public void setNumC(int numC) {
+        this.numC = numC;
+    }
+
+    public float getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(float saldo) {
+        this.saldo = saldo;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(int idCliente) {
+        this.idCliente = idCliente;
+    }
     
 }
